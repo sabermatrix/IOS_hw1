c10df945f5f9c5e989ef095d26af664335a6b8f3
“Your <cherry880807>
2020-03-18 22:18:50 +0800

    Initial Commit

---
+++ IOS_hw1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IOS_hw1
+//
+//  Created by User03 on 2020/3/18.
+//  Copyright © 2020 test. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
